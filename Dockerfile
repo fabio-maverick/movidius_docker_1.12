ba64abb670738415259ba7fe32ba681dc1f50d50
fabio-maverick <fabio.magalhaes@gmail.com>
2018-06-10 18:28:57 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM nvidia/cuda:9.0-cudnn7-devel-ubuntu16.04
-MAINTAINER Fábio Magalhães <fabio.magalhaes@gmail.com>
+FROM ubuntu:16.04
+MAINTAINER Fabio Magalhaes<fabio.magalhaes@gmail.com>
 
 ENV PYTHONPATH /opt/movidius/mvnc/python:${PYTHONPATH}
 ARG TF_VERSION
@@ -12,35 +12,31 @@ RUN apt-get install -y sudo
 RUN apt-get install -y nano
 RUN apt-get install -y lsb-release
 RUN apt-get install -y python3-pip
-RUN apt-get install -y zip
-RUN apt-get install -y unzip
 
 RUN git clone https://github.com/movidius/ncsdk.git /ncsdk
 RUN git clone https://github.com/movidius/ncappzoo.git /ncappzoo
 
 RUN pip install --upgrade pip
-# RUN pip install tensorflow-gpu==1.4.1
+#RUN pip install tensorflow-gpu==1.4.1
 RUN pip3 install --upgrade pip
-
-# RUN pip3 install opencv-python
-# RUN pip3 install opencv-contrib-python
-
-RUN cd /ncappzoo/apps/video_objects && ./install-opencv-from_source.sh
+RUN pip3 install opencv-python
+RUN pip3 install opencv-contrib-python
 
 WORKDIR /ncsdk
 RUN make install
 #RUN make examples
 
 # Go to NCSDK root
-WORKDIR /
-RUN git clone https://github.com/tensorflow/tensorflow.git tf-${TF_VERSION}
-RUN git clone https://github.com/tensorflow/models.git tf-models
-RUN cd tf-${TF_VERSION} && git checkout ${TF_VERSION} && cd ..
-ENV TF_SRC_PATH=/tf-${TF_VERSION}
-ENV TF_MODELS_PATH=/tf-models
+#WORKDIR /
+#RUN git clone https://github.com/tensorflow/tensorflow.git tf-${TF_VERSION}
+#RUN git clone https://github.com/tensorflow/models.git tf-models
+#RUN cd tf-${TF_VERSION} && git checkout ${TF_VERSION} && cd ..
+#ENV TF_SRC_PATH=/tf-${TF_VERSION}
+#ENV TF_MODELS_PATH=/tf-models 
 
 # we only check with tensorflow example
-WORKDIR /ncappzoo/tensorflow
+#WORKDIR /ncappzoo/tensorflow
+WORKDIR /ncappzoo/apps
 #RUN make
 
 CMD ["bash"]
--- run_movidius_docker.sh
+++ run_movidius_docker.sh
@@ -3,9 +3,9 @@
 # Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
 # Full license terms provided in LICENSE.md file.
 
-REDTAIL_NAME=$1
-if [[ -z "${REDTAIL_NAME}" ]]; then
-    REDTAIL_NAME=redtail-sim-v2
+NCSDK_NAME=$1
+if [[ -z "${NCSDK_NAME}" ]]; then
+    NCSDK_NAME=movidius_sdk
 fi
 
 HOST_DATA_DIR=$2
@@ -18,25 +18,25 @@ if [[ -z "${CONTAINER_DATA_DIR}" ]]; then
     CONTAINER_DATA_DIR=/data/
 fi
 
-echo "Container name    : ${REDTAIL_NAME}"
+echo "Container name    : ${NCSDK_NAME}"
 echo "Host data dir     : ${HOST_DATA_DIR}"
 echo "Container data dir: ${CONTAINER_DATA_DIR}"
-REDTAIL_ID=`docker ps -aqf "name=^/${REDTAIL_NAME}$"`
-if [ -z "${REDTAIL_ID}" ]; then
+NCSDK_ID=`docker ps -aqf "name=^/${NCSDK_NAME}$"`
+if [ -z "${NCSDK_ID}" ]; then
     echo "Creating new redtail container."
     xhost +
-    nvidia-docker run -it --privileged --network=host -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix${DISPLAY} -p 14556:14556/udp --name=${REDTAIL_NAME} nvidia-redtail-sim:kinetic-v2 bash
+    nvidia-docker run -it --privileged --network=host -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix${DISPLAY} -p 14556:14556/udp --name=${NCSDK_NAME} ncsdk:ubuntu16.04 bash
 else
-    echo "Found redtail container: ${REDTAIL_ID}."
+    echo "Found redtail container: ${NCSDK_ID}."
     # Check if the container is already running and start if necessary.
-    if [ -z `docker ps -qf "name=^/${REDTAIL_NAME}$"` ]; then
-        xhost +local:${REDTAIL_ID}
-        echo "Starting and attaching to ${REDTAIL_NAME} container..."
-        docker start ${REDTAIL_ID}
-        docker attach ${REDTAIL_ID}
+    if [ -z `docker ps -qf "name=^/${NCSDK_NAME}$"` ]; then
+        xhost +local:${NCSDK_ID}
+        echo "Starting and attaching to ${NCSDK_NAME} container..."
+        docker start ${NCSDK_ID}
+        docker attach ${NCSDK_ID}
     else
-        echo "Found running ${REDTAIL_NAME} container, attaching bash..."
-        docker exec -it ${REDTAIL_ID} bash
+        echo "Found running ${NCSDK_NAME} container, attaching bash..."
+        docker exec -it ${NCSDK_ID} bash
     fi
 fi
 
